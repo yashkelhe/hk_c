871df971717c802e27d34e637de2a0129e0cf3ee
yash kelhe <yashkelhe30@gmail.com>
2024-07-08 20:09:58 +0530

    tailwind

---
+++ week_8_Tailwind/youtube_Clone/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center text-xl text-red-400 w-40 h-40 items-center bg-slate-400 rounded-full">
+        text
+      </div>
+    </>
+  );
+}
+
+export default App;
